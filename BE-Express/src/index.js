f43d5b33059a690ba5b6efdae1447a23a36ad166
Satrio Kamaludin <satriokamaludin@gmail.com>
2024-08-16 15:52:21 +0700

    Express CORS

--- BE-Express/src/index.js
+++ BE-Express/src/index.js
@@ -4,6 +4,20 @@ const apiRoutes = require('./routes/api');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Enable CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 // Middleware to parse JSON requests data
 app.use(express.json());
 
